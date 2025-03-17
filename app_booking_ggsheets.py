99f625ff1581bceafc104d01d7c0f725d09a0ce1
duytadinh98@gmail.com <duytadinh98@gmail.com>
2025-03-17 22:04:23 +0700

    first commit

---
+++ app_booking_ggsheets.py
@@ -0,0 +1,190 @@
+import streamlit as st
+from function_file import read_gg_sheets
+from streamlit_gsheets import GSheetsConnection
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+import os
+import time
+import calendar
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from function_file import preprocessing_data_calendar, generate_hour_block, next_weekday_in_interval_lst, color_value, mapping_dow 
+
+st.set_page_config(page_icon=":calendar:")
+
+conn = st.connection("gsheets", type=GSheetsConnection, ttl=5)
+data_template = conn.read(ttl=3)
+# output_booking = "StorageFolder/bookings.csv"
+
+if data_template.shape[1] == 9:
+    st.session_state.bookings = conn.read(ttl=3)
+else:
+    st.session_state.bookings = pd.DataFrame(columns=['OrderTime', 'CustomerID', 'CustomerType', 'StartTime', 'EndTime', 'StartDate', 'EndDate', 'DayOfWeek', 'CourtNumber'])
+
+
+current_datetime = datetime.now().date()
+page = st.sidebar.selectbox("Choose a page", ["Booking", "Calendar"])
+
+if page == 'Booking':
+    st.markdown(
+    """
+    <style>
+    .main .block-container {
+        max-width: 60%;}
+    </style>
+    """,
+    unsafe_allow_html=True)
+
+
+    st.markdown("<h1 style='text-align: center; color: red;'>THÔNG TIN ĐẶT SÂN</h1>", unsafe_allow_html=True)
+    with st.form("booking_form"):
+        col1, col2 = st.columns(2)
+        with col1:
+            order_date = st.date_input("Order Date", value = current_datetime, format = "YYYY-MM-DD")
+            customer_id = st.text_input("Customer ID")
+            customer_type = st.selectbox("Customer Type", ["Cố Định", "Vãng Lai"], index=None)
+            court_num = st.selectbox("Court Number", ["Court 1", "Court 2", "Court 3"], index = None)
+            tba_box = st.text("To Be Added")
+
+        with col2:
+            day_of_week = st.multiselect("Day of Week", ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], key="test_group_1")
+            start_date = st.date_input("Start Date")
+            end_date = st.date_input("End Date")
+            start_time = st.selectbox("Start Time (Hour)", options=range(6, 23), index=0) 
+            end_time = st.selectbox("End Time (Hour)", options=range(7, 24), index=0) 
+
+        submitted = st.form_submit_button("Book Court")
+
+    if submitted:
+        with st.spinner("Processing your booking..."):
+            time.sleep(1)  
+        invalid_days = [day for day in day_of_week if not any((start_date + timedelta(days=i)).strftime("%A") == day for i in range((end_date - start_date).days + 1))]
+        if invalid_days:
+            st.error(f"The selected {invalid_days} do not exist within the date range {start_date} to {end_date}.", icon="🚨")
+
+        elif not all([customer_id, customer_type, start_time, end_time, start_date, end_date, day_of_week, court_num]):
+            st.error("Please fill out all fields.")
+        elif end_date < start_date or end_time <= start_time:
+            st.error("End Time must be later than Start Time.")
+        elif not day_of_week:
+            st.error("Please select at least one day.")
+
+        # Check if the selected days of the week exist within the date range
+        else:
+            day_of_week_str = "_".join(day_of_week)
+            # Check if the time slot is occupied for any of the selected days
+            is_occupied = False
+            for day in day_of_week:
+                occupied = st.session_state.bookings[
+                    (st.session_state.bookings['DayOfWeek'].str.contains(day)) & 
+                    (
+                        ((st.session_state.bookings['StartDate'] <= datetime.strftime(start_date, "%Y-%m-%d")) & (st.session_state.bookings['EndDate'] >= datetime.strftime(start_date, "%Y-%m-%d"))) |
+                        ((st.session_state.bookings['StartDate'] <= datetime.strftime(end_date, "%Y-%m-%d")) & (st.session_state.bookings['EndDate'] >= datetime.strftime(end_date, "%Y-%m-%d"))) |
+                        ((st.session_state.bookings['StartDate'] >= datetime.strftime(start_date, "%Y-%m-%d")) & (st.session_state.bookings['EndDate'] <= datetime.strftime(end_date, "%Y-%m-%d")))
+                    ) & 
+                    (
+                        (st.session_state.bookings['StartTime'] < end_time) & 
+                        (st.session_state.bookings['EndTime'] > start_time) &
+                        (st.session_state.bookings['CourtNumber'] == court_num) 
+                    )
+                ].any().any()
+                if occupied:
+                    is_occupied = True
+                    break
+
+            if is_occupied:
+                st.error("This time slot is already occupied for one or more selected days. Please choose another time.")
+            else:
+                # Add the booking to the DataFrame
+                new_booking = pd.DataFrame({
+                    'OrderTime': [order_date],
+                    'CustomerID': [customer_id],
+                    'CustomerType': [customer_type],
+                    'StartTime': [start_time],
+                    'EndTime': [end_time],
+                    'StartDate': [start_date],
+                    'EndDate': [end_date],
+                    'DayOfWeek': [day_of_week_str],
+                    'CourtNumber' : [court_num]
+                })
+                st.session_state.bookings = pd.concat([st.session_state.bookings, new_booking], ignore_index=True)
+                conn.update(worksheet="InputData", data=st.session_state.bookings)
+                success_message = st.success("Court booked successfully!")
+                time.sleep(2)
+                success_message.empty()
+            
+elif page == 'Calendar':
+    st.markdown(
+        """
+        <style>
+        .main .block-container {max-width: 80%;}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.markdown("<h1 style='text-align: center; color: green;'>DỮ LIỆU ĐẶT SÂN</h1>", unsafe_allow_html=True)
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        calendar_type = st.selectbox("Hello", ["Filter Data", "All Data"], index = 1)
+
+    tab2_1, tab2_2, tab2_3 = st.tabs(["Calendar", "Kiểm tra Lịch", "Tải dữ liệu"])
+
+    start_date_input = (current_datetime .replace(day = 1)).strftime('%Y-%m-%d')
+    end_date_input = (current_datetime.replace(day = 1)  + relativedelta(day = 31)).strftime('%Y-%m-%d')
+
+    df_raw, df_calendar, df_calendar_count = preprocessing_data_calendar(st.session_state.bookings, data_type = calendar_type, 
+                                                                         start_date_calendar=start_date_input,end_date_calendar=end_date_input)
+
+    
+    with tab2_1:
+        if 'show_table_1' not in st.session_state:
+            st.session_state.show_table_1 = True
+        if st.button("Switch Table"):
+            st.session_state.show_table_1 = not st.session_state.show_table_1
+            
+        if st.session_state.show_table_1:
+            st.dataframe(df_calendar_count)
+        else:
+            st.dataframe(df_calendar)
+
+    with tab2_2:
+        col1, col2, col3 = st.columns(3)
+        ###----------------Set Up Filter----------------###
+        with col1:
+            lst_date_range = st.date_input("Select Date Range", (current_datetime, current_datetime))
+            
+        with col2:
+            lst_hour_block = st.multiselect("Select Hour Block", generate_hour_block(6,22))
+        with col3:
+            dow_filter = st.multiselect("Day of Week", ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+
+        if isinstance(lst_date_range, (list, tuple)) and len(lst_date_range) == 2:
+            start_date_filter, end_date_filter = lst_date_range
+            lst_date_filter = next_weekday_in_interval_lst(start_date_filter, end_date_filter)
+        else:
+            st.stop()
+
+        st.markdown("<br>"*2, unsafe_allow_html=True)
+        ###---------------------------------------------###
+        if dow_filter != []:
+            lst_date_filter = next_weekday_in_interval_lst(start_date_filter, end_date_filter,  [mapping_dow[t] for t in dow_filter if t != "All"])
+
+        cond_filter =  (df_raw.DeliverDate.isin(lst_date_filter))
+        if lst_hour_block != []:
+            cond_filter = (df_raw.DeliverDate.isin(lst_date_filter)) & (df_raw.HourBlock.isin(lst_hour_block))
+
+        data_calendar_check = pd.pivot_table(df_raw.loc[cond_filter], index = 'DeliverDate', columns = 'HourBlock', \
+                                        values = 'CourtNumber', aggfunc = 'count').reset_index()
+        data_calendar_check.fillna(0, inplace = True)
+        data_calendar_check_styled = data_calendar_check.style.map(color_value).format('{:.0f}', subset=data_calendar_check.select_dtypes(include=['number']).columns)
+        
+        st.dataframe(data_calendar_check_styled)
+    with tab2_3:
+        st.title('InputData')
+        st.dataframe(st.session_state.bookings)
+        st.title('RawData')
+        st.dataframe(df_raw)
---
+++ function_file.py
@@ -0,0 +1,109 @@
+from datetime import datetime, timedelta
+import hashlib
+import pandas as pd
+import numpy as np
+import gspread
+
+mapping_dow = {'Monday': 0,'Tuesday': 1,'Wednesday': 2,'Thursday': 3,'Friday': 4,'Saturday': 5,'Sunday': 6}
+
+def read_gg_sheets(cred_location, wb_url, ws_name):
+    gc = gspread.service_account(filename=cred_location)
+    wb = gc.open_by_url(wb_url)
+    ws = wb.worksheet(ws_name)
+    data = pd.DataFrame(ws.get_all_records())
+    return data
+
+
+def next_weekday_in_interval_lst(start_date_input, end_date_input, target_weekdays=np.arange(0,7)):
+    lst_weekday = []
+    if isinstance(start_date_input, str) and isinstance(end_date_input, str):
+        start_date = datetime.strptime(start_date_input, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date_input, '%Y-%m-%d')
+    else:
+        start_date, end_date = start_date_input, end_date_input
+    # Iterate through each day in the interval
+    current_date = start_date
+    while current_date <= end_date:
+        # Check if the current date's weekday is in the target weekdays list
+        if current_date.weekday() in target_weekdays:
+            lst_weekday.append(current_date.strftime("%Y-%m-%d"))
+        # Move to the next day
+        current_date += timedelta(days=1)
+    return lst_weekday
+
+def next_weekday_in_interval(start_date_input, end_date_input, target_weekday_input):
+    start_date = datetime.strptime(start_date_input, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date_input, '%Y-%m-%d')
+    target_weekday = int(target_weekday_input)
+    
+    days_until_target = (target_weekday - start_date.weekday()) % 7
+    next_date = start_date + timedelta(days=days_until_target)
+
+    matching_dates = []
+    while next_date <= end_date:
+        matching_dates.append(next_date.strftime('%Y-%m-%d'))
+        next_date += timedelta(days=7)
+
+    return matching_dates
+
+def generate_hexacode(row):
+    combined_string = ''.join(map(str, row))
+    hash_object = hashlib.sha256(combined_string.encode('utf-8'))
+    hex_dig = hash_object.hexdigest()
+    hexacode = 'ord_' + hex_dig[:6]
+    return hexacode
+
+def generate_hour_block(start_time, end_time):
+    time_range = pd.date_range(start=f'{int(start_time)}:00', end=f'{int(end_time)}:00', freq='H')
+    return [f'{start.hour:02d}-{end.hour:02d}' for start, end in zip(time_range[:-1], time_range[1:])]
+
+def color_value(val):
+    if val == 3:
+        bg_color = "red"
+        txt_color = "white"
+    elif val == 0:
+        bg_color = "#00C000"
+        txt_color = "white"
+    else:
+        bg_color = "white"
+        txt_color = ""
+    return f'color:{txt_color}; background-color: {bg_color};'
+
+
+
+def preprocessing_data_calendar(data_frame, start_date_calendar, end_date_calendar, data_type = 'Filter Data'):
+    data = pd.DataFrame(data_frame, dtype=str)
+    if data.shape[0] >0:
+        data[['StartTime', 'EndTime']] = data[['StartTime', 'EndTime']].apply(pd.to_numeric)
+        data['OrderID'] = data.drop(['CustomerType'], axis = 1).apply(generate_hexacode, axis = 1)
+        data['DayOfWeek_Split'] = data['DayOfWeek'].str.split('_')
+        data = data.explode('DayOfWeek_Split')
+        data['DayOfWeek_Split'].replace(mapping_dow, inplace = True)
+        data['DeliverDate'] = data.apply(lambda row: next_weekday_in_interval(row['StartDate'], row['EndDate'], row['DayOfWeek_Split']), axis = 1)
+        data = data.explode('DeliverDate')
+        data['HourBlock'] = data.apply(lambda row: generate_hour_block(row['StartTime'], row['EndTime']), axis=1)
+        data = data.explode('HourBlock').sort_values(by = ['OrderID', 'DeliverDate', 'HourBlock']).drop(['DayOfWeek_Split'], axis = 1).reset_index(drop=True)
+
+        if data_type == 'All Data':
+            data_calendar_hour_block = pd.DataFrame({'DeliverDate':pd.date_range(start_date_calendar, end_date_calendar)}, dtype=str)\
+                                    .merge(pd.DataFrame({'HourBlock': generate_hour_block(6,23)}, dtype = str), how = 'cross')
+            data = data_calendar_hour_block.merge(data, how = 'left', left_on = ['DeliverDate', 'HourBlock'], right_on = ['DeliverDate', 'HourBlock'])
+        
+        column_deliver_date, column_hour_block = ['DeliverDate', 'HourBlock']
+        ###----------------------------------------------------------------###
+        data_pivot = pd.pivot_table(data, index = [column_deliver_date], columns = column_hour_block, values = 'CustomerID', \
+                                    aggfunc = lambda x: ' || '.join( sorted(x.fillna(''))) ).reset_index()
+        
+        data_pivot_count = pd.pivot_table(data, index = column_deliver_date, columns = column_hour_block, values = 'CustomerID', aggfunc = 'count')
+        data_pivot_count.fillna(0,inplace = True)
+        data_pivot_count_styled = data_pivot_count.style.map(color_value).format('{:.0f}')
+        ###----------------------------------------------------------------###
+    else:
+        raise Exception("Cannot read the Dataframe")
+    
+    return data, data_pivot, data_pivot_count_styled
+
+
+
+
+
